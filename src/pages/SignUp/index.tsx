e869e70d4981d60fd72962b416bf49c158b78597
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-08 18:36:58 -0300

    Estilizando inputs

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiMail, FiUser, FiArrowLeft, FiLock } from 'react-icons/fi'
+import { Form } from '@unform/web'
 
 
 import { Container, Content, Background } from './styles' 
@@ -8,27 +9,34 @@ import logoImg from '../../assets/logo.svg'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background/>
-        <Content>
-            <img src={logoImg} alt="GoBarber"/>
+const SignUp: React.FC = () => {
 
-            <form>
-                <h1>Faça seu cadastro</h1>
+    function handleSubmit(data: object): void{
+        console.log(data);
+    }
 
-                <Input name="email" icon={FiUser} placeholder="Nome"/>
-                <Input name="email" icon={FiMail} placeholder="Email"/>
-                <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="login">
-                <FiArrowLeft/>
-                Voltar para logon
-            </a>
-
-        </Content>
-    </Container>
-);
+    return(
+        <Container>
+            <Background/>
+            <Content>
+                <img src={logoImg} alt="GoBarber"/>
+    
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+    
+                    <Input name="email" icon={FiUser} placeholder="Nome"/>
+                    <Input name="email" icon={FiMail} placeholder="Email"/>
+                    <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="login">
+                    <FiArrowLeft/>
+                    Voltar para logon
+                </a>
+    
+            </Content>
+        </Container>
+    );
+}
 
 export default SignUp;
