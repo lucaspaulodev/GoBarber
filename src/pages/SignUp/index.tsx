fc5064c7d6aed271c7b21ce2e112c8993271981c
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-08 17:28:07 -0300

    SignUp Page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FiMail, FiUser, FiArrowLeft, FiLock } from 'react-icons/fi'
+
+
+import { Container, Content, Background } from './styles' 
+import logoImg from '../../assets/logo.svg'
+
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+
+const SignUp: React.FC = () => (
+    <Container>
+        <Background/>
+        <Content>
+            <img src={logoImg} alt="GoBarber"/>
+
+            <form>
+                <h1>Faça seu cadastro</h1>
+
+                <Input name="email" icon={FiUser} placeholder="Nome"/>
+                <Input name="email" icon={FiMail} placeholder="Email"/>
+                <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+                <Button type="submit">Cadastrar</Button>
+            </form>
+            <a href="login">
+                <FiArrowLeft/>
+                Voltar para logon
+            </a>
+
+        </Content>
+    </Container>
+);
+
+export default SignUp;
