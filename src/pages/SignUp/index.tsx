ec68f26d12ac1a7ff67106b49d634ac044040a22
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-13 16:20:48 -0300

    Adding validation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { FiMail, FiUser, FiArrowLeft, FiLock } from 'react-icons/fi'
 import { Form } from '@unform/web'
+import * as Yup from 'yup';
 
 
 import { Container, Content, Background } from './styles' 
@@ -11,9 +12,21 @@ import Input from '../../components/Input'
 
 const SignUp: React.FC = () => {
 
-    function handleSubmit(data: object): void{
-        console.log(data);
-    }
+    const handleSubmit = useCallback(async (data: object) => {
+        try{
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required('Email obrigatório').email('Digite um email válido'),
+                password: Yup.string().min(6, 'A senha deve ter no mínimo 6 digitos'),
+            })
+
+            await schema.validate(data, {
+                abortEarly: false,
+            })
+        } catch(err) {
+            console.log(err);
+        }
+    }, [])
 
     return(
         <Container>
