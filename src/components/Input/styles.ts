92288609bd143f7384df86390e8375137b39cff4
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-13 18:48:17 -0300

    Inputs validations done

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, {css} from 'styled-components'
+import Tooltip from '../Tooltip'
 
 interface ContainerProps {
     isFilled: boolean;
     isFocused: boolean;
+    isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,17 +24,24 @@ export const Container = styled.div<ContainerProps>`
     }
 
     ${(props) => 
+        props.isErrored && 
+        css`
+            border-color: #c53030;
+    `}
+
+    ${(props) => 
         props.isFocused && 
         css`
-        color: #ff9000;
-        border-color: #ff9000;
+            color: #ff9000;
+            border-color: #ff9000;
     `}
 
     ${(props) => 
         props.isFilled && 
         css`
-        color: #ff9000;
+            color: #ff9000;
     `}
+
 
     input{
         flex: 1;
@@ -47,6 +56,23 @@ export const Container = styled.div<ContainerProps>`
 
     svg{
         margin-right: 16px;
-        color: #666360;
     }
-`
+`;
+
+export const Error = styled(Tooltip)`
+    height: 20px;
+    margin-left: 16px;
+
+    svg {
+        margin: 0;
+    }
+
+    span {
+        background: #c53030;
+        color: #fff;
+
+        &::before {
+            border-color: #c53030 transparent;
+        }
+    }
+`;
