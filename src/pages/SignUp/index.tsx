7e628544b556a2861952d9e0e8beb8e8f3adc997
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-13 16:43:16 -0300

    Adding input errors messages

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,25 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useRef} from 'react';
 import { FiMail, FiUser, FiArrowLeft, FiLock } from 'react-icons/fi'
+import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors'
 
 
-import { Container, Content, Background } from './styles' 
 import logoImg from '../../assets/logo.svg'
 
-import Button from '../../components/Button'
 import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Content, Background } from './styles' 
 
 const SignUp: React.FC = () => {
 
+    const formRef = useRef<FormHandles>(null);
+
     const handleSubmit = useCallback(async (data: object) => {
         try{
+            formRef.current?.setErrors({})
             const schema = Yup.object().shape({
                 name: Yup.string().required('Nome obrigatório'),
                 email: Yup.string().required('Email obrigatório').email('Digite um email válido'),
@@ -23,8 +29,10 @@ const SignUp: React.FC = () => {
             await schema.validate(data, {
                 abortEarly: false,
             })
+
         } catch(err) {
-            console.log(err);
+            const errors = getValidationErrors(err)
+            formRef.current?.setErrors(errors)
         }
     }, [])
 
@@ -34,10 +42,10 @@ const SignUp: React.FC = () => {
             <Content>
                 <img src={logoImg} alt="GoBarber"/>
     
-                <Form onSubmit={handleSubmit}>
+                <Form ref={formRef} onSubmit={handleSubmit}>
                     <h1>Faça seu cadastro</h1>
     
-                    <Input name="email" icon={FiUser} placeholder="Nome"/>
+                    <Input name="name" icon={FiUser} placeholder="Nome"/>
                     <Input name="email" icon={FiMail} placeholder="Email"/>
                     <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
                     <Button type="submit">Cadastrar</Button>
@@ -46,7 +54,6 @@ const SignUp: React.FC = () => {
                     <FiArrowLeft/>
                     Voltar para logon
                 </a>
-    
             </Content>
         </Container>
     );
