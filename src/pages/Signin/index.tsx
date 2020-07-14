56336fc8b0d3e7594563f448255de4a847cf890d
Lucas Paulo <lucaspaulodev@gmail.com>
2020-07-13 21:52:15 -0300

    Updating hooks

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,10 +1,10 @@
-import React, {useRef, useCallback, useContext} from 'react';
+import React, {useRef, useCallback} from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 
-import AuthContext from '../../context/AuthContext'
+import { useAuth } from '../../hooks/AuthContext'
 import getValidationErrors from '../../utils/getValidationErrors'
 
 
@@ -16,12 +16,18 @@ import Input from '../../components/Input'
 
 import { Container, Content, Background } from './styles'
 
-const SignIn: React.FC = () => {
 
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
+const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
+    const {signIn} = useAuth();
 
-    const handleSubmit = useCallback(async (data: object) => {
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
         try{
             formRef.current?.setErrors({})
             const schema = Yup.object().shape({
@@ -33,11 +39,15 @@ const SignIn: React.FC = () => {
                 abortEarly: false,
             })
 
+            signIn({
+                email: data.email,
+                password: data.password
+            })
         } catch(err) {
             const errors = getValidationErrors(err)
             formRef.current?.setErrors(errors)
         }
-    }, [])
+    }, [signIn])
 
     return (
         <Container>
